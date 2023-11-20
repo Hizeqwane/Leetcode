be8b7ece9aa6efdf47d55cc316f52b043d88570c
Егор Вишняков <vishnya997@yandex.ru>
2023-11-20 18:25:10 +0300

    Search in Rotated Sorted Array

---
+++ 33. Search in Rotated Sorted Array/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    Console.WriteLine("Введите массив через запятую.");
+    var nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+    Console.WriteLine("Введите target.");
+    var target = int.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+    
+    var d1 = DateTime.Now;
+    var result = Search(nums, target);
+    var d2 = DateTime.Now;
+
+    Console.WriteLine("Время выполнения: {0}", d2 - d1);
+    Console.Write($"{nameof(Search)}: {result}");
+    
+    Console.WriteLine();
+}
+
+int Search(int[] nums, int target)
+{
+    return Array.IndexOf(nums, target);
+}

c6e67cd4a1384fa662e93b4a4c2b5f7a8b0f41ef
Егор Вишняков <vishnya997@yandex.ru>
2023-12-07 16:22:46 +0300

    Add Jump Game II & Permutations

---
+++ 45. Jump Game II/Program.cs
@@ -0,0 +1,67 @@
+﻿while (true)
+{
+    string date = "29.05.83";
+    var d = DateTime.Parse(date);
+    var t = DateTime.TryParse(date, out _);
+
+    Console.WriteLine("Введите массив через запятую.");
+    var nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+    Console.WriteLine();
+    
+    var d1 = DateTime.Now;
+    var result = Jump(nums);
+    var d2 = DateTime.Now;
+
+    Console.WriteLine("Время выполнения: {0}", d2 - d1);
+    Console.Write($"{nameof(Jump)}: {result}");
+    
+    Console.WriteLine();
+}
+
+int Jump(int[] nums)
+{
+    int n = nums.Length;
+    int prevMax = 0;
+    int max = 0;
+    int steps = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        max = Math.Max(max, i + nums[i]);
+        if (i == prevMax)
+        {
+            steps++;
+            prevMax = max;
+        }
+    }
+    return steps;
+}
+
+int JumpMy(int[] nums)
+{
+    return GetBest(nums, 0);
+}
+
+int GetBest(int[] nums, int startInd)
+{
+    if (startInd == nums.Length - 1) 
+        return 0;
+    if (nums[startInd] >= nums.Length - startInd - 1)
+        return 1;
+
+    var endForFinding = Math.Min(nums.Length - 1, startInd + nums[startInd]);
+
+    var maxStep = 0;
+    var indMax = 0;
+    for (int i = startInd + 1; i <= endForFinding; i++)
+    {
+        var currentAvailableStep = nums[i] + (i - startInd - 1);
+        if (currentAvailableStep > maxStep)
+        {
+            maxStep = currentAvailableStep;
+            indMax = i;
+        }
+    }
+
+    return 1 + GetBest(nums, indMax);
+}
---
+++ 46. Permutations/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+
+while (true)
+{
+    Console.WriteLine("Введите массив через запятую");
+    var nums = Console.ReadLine().Split(",").Select(s => int.Parse(s)).ToArray();
+
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+    var result = Permute(nums);
+    stopWatch.Stop();
+
+    Console.WriteLine("Permute: Время выполнения: {0}", stopWatch.Elapsed);
+    foreach (var res in result) 
+        Console.WriteLine(string.Join(" , ", res));
+
+    Console.WriteLine();
+
+    stopWatch.Restart();
+    result = PermuteMy(nums);
+    stopWatch.Stop();
+
+    Console.WriteLine("MyPermute: Время выполнения: {0}", stopWatch.Elapsed);
+    foreach (var res in result)
+        Console.WriteLine(string.Join(" , ", res));
+}
+
+IList<IList<int>> Permute(int[] nums)
+{
+    var result = new List<IList<int>>();
+    var visited = new bool[nums.Length];
+    BackTrack(nums, new List<int>(), visited, result);
+    return result;
+}
+
+void BackTrack(int[] nums, List<int> current, bool[] visited, IList<IList<int>> result)
+{
+    if (current.Count == nums.Length)
+    {
+        result.Add(new List<int>(current));
+        return;
+    }
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (visited[i]) { continue;}
+
+        visited[i] = true;
+        current.Add(nums[i]);
+        BackTrack(nums, current, visited, result);
+        current.Remove(nums[i]);
+        visited[i] = false;
+    }
+}
+
+
+
+#region Cheat with using Task №31
+IList<IList<int>> PermuteMy(int[] nums)
+{
+    if (nums.Length == 1)
+        return new List<IList<int>> { nums.ToList() };
+
+    var hash = new HashSet<string>();
+    while (hash.Add(string.Join(',', nums)))
+    {
+        NextPermutation(nums);
+    }
+
+    var result = new List<IList<int>>();
+    foreach (var h in hash)
+        result.Add(h.Split(',').Select(int.Parse).ToList());
+
+    return result;
+}
+
+void NextPermutation(int[] nums)
+{
+    if (nums.Length < 2)
+        return;
+
+    if (nums[^1] > nums[^2])
+    {
+        (nums[^1], nums[^2]) = (nums[^2], nums[^1]);
+        return;
+    }
+
+    var end = nums.Length - 1;
+    var index = -1;
+    for (var i = end - 1; i > -1; i--)
+        if (nums[i] < nums[i + 1])
+        {
+            index = i;
+            break;
+        }
+
+    if (index == -1)
+    {
+        Array.Reverse(nums);
+        return;
+    }
+
+    var indexForReplace = -1;
+    var min = int.MaxValue;
+    for (var i = end; i >= index + 1; i--)
+        if (nums[i] > nums[index] && nums[i] < min)
+        {
+            min = nums[i];
+            indexForReplace = i;
+        }
+
+    (nums[index], nums[indexForReplace]) = (nums[indexForReplace], nums[index]);
+    Array.Reverse(nums, index + 1, end - index);
+} 
+#endregion

703fed1ef609e4c70d6a0741fb34612e10254aac
Егор Вишняков <vishnya997@yandex.ru>
2024-01-29 13:55:54 +0300

    add streets flag

---
+++ 55. Jump Game/Program.cs
@@ -0,0 +1,30 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите массив через запятую.");
+    var nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+    Console.WriteLine();
+    
+    var d1 = DateTime.Now;
+    var result = CanJump(nums);
+    var d2 = DateTime.Now;
+
+    Console.WriteLine("Время выполнения: {0}", d2 - d1);
+    Console.Write($"{nameof(CanJump)}: {result}");
+    
+    Console.WriteLine();
+}
+
+bool CanJump(int[] nums)
+{
+    var n = nums.Length;
+    var max = 0;
+    for (var i = 0; i < n - 1; i++)
+    {
+        if (max >= i + nums[i] && i == max)
+            return false;
+
+        max = Math.Max(max, nums[i] + i);
+    }
+    return true;
+}
---
+++ 56. Merge Intervals/Program.cs
@@ -0,0 +1,82 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите массивы (все числа через запятую).");
+    var nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+    Console.WriteLine();
+
+    var intervals = new int[nums.Length / 2][];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        intervals[i / 2] ??= new int[2];
+        if (i % 2 == 0)
+            intervals[i / 2][0] = nums[i];
+        else
+            intervals[i / 2][1] = nums[i];
+    }
+
+    var d1 = DateTime.Now;
+    var result = Merge(intervals);
+    var d2 = DateTime.Now;
+
+    Console.WriteLine("Время выполнения: {0}", d2 - d1);
+    Console.Write($"{nameof(Merge)}: {result}");
+
+    Console.WriteLine();
+}
+
+int[][] Merge(int[][] intervals)
+{
+    var result = new List<int[]>();
+
+    foreach (var interval in intervals)
+    {
+        var start = interval[0];
+        var end = interval[1];
+
+        var wasFounded = false;
+        foreach (var resInterval in result)
+        {
+            var resStart = resInterval[0];
+            var resEnd = resInterval[1];
+
+            if (start > resStart && end < resEnd)
+            {
+                wasFounded = true;
+                break;
+            }
+
+            if (start > resStart && end > resEnd)
+            {
+                resInterval[1] = end;
+
+                wasFounded = true;
+                break;
+            }
+
+            if (start < resStart && end < resEnd)
+            {
+                resInterval[0] = start;
+
+                wasFounded = true;
+                break;
+            }
+
+            if (start < resStart && end > resEnd)
+            {
+                resInterval[0] = start;
+                resInterval[1] = end;
+
+                wasFounded = true;
+                break;
+            }
+        }
+
+        if (!wasFounded)
+        {
+            result.Add(new []{start, end});
+        }
+    }
+
+    return result.ToArray();
+}

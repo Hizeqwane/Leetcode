1430b2bdaaf1ce7c83600383cd01ff0cad96e636
Vishnyakov.E.A <Vishnyakov.E.A@sbertech.ru>
2025-02-10 21:00:18 +0300

    Sqrt

---
+++ 69. Sqrt/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+var stopWatch = Stopwatch.StartNew();
+var result = MySqrt(15);
+stopWatch.Stop();
+
+Console.WriteLine();
+Console.WriteLine($"{nameof(MySqrt)}: Время выполнения: {stopWatch.Elapsed}");
+
+Console.WriteLine(result);
+
+Console.WriteLine();
+
+int MySqrt(int x) {
+    if (x == 0)
+        return 0;
+
+    int left = 1;
+    int right = x;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        int sqrt = x / mid;
+
+        if (sqrt == mid)
+            return mid;
+        else if (sqrt < mid)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+
+    return right;
+}
---
+++ 71. Simplify Path/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+Dictionary<string, string> tests = new()
+{
+    {"/home/", "/home"},
+    {"/home//foo/", "/home/foo"},
+    {"/home/user/Documents/../Pictures", "/home/user/Pictures"},
+    {"/../", "/"},
+    {"/.../a/../b/c/../d/./", "/.../b/d"},
+};
+
+foreach (var test in tests)
+{
+    var stopWatch = Stopwatch.StartNew();
+    var result = SimplifyPath(test.Key);
+    stopWatch.Stop();
+
+    Console.WriteLine();
+    Console.WriteLine($"{nameof(SimplifyPath)}: Время выполнения: {stopWatch.Elapsed}");
+
+    var isOk = result == test.Value ? "OK" : "Failed";
+    Console.WriteLine($"{result} - {test.Value} -> {isOk}");
+
+    Console.WriteLine();   
+}
+
+string SimplifyPath(string path)
+{
+    return "";
+}
